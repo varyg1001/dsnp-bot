63428dd30b82aed946b0cb9758c130c372bfb198
varyg <88599103+varyg1001@users.noreply.github.com>
2023-07-09 20:18:43 +0200

    test

--- dsnp-bot/disney.py
+++ dsnp-bot/disney.py
@@ -73,7 +73,7 @@ class Data():
         else:
             bar = f"{filled_char * int(filled_length)}{empty_char * (10 - int(filled_length))}"
 
-        return "█" + bar + "█"
+        return bar
 
     @property
     def render(self) -> str:
