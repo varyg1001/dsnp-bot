cd3587121611515351d3993713b8d4a32931b44c
varyg <varygb11@gmail.com>
2025-04-03 16:05:54 +0200

    Fix aiogram for v3

--- dsnpbot/__main__.py
+++ dsnpbot/__main__.py
@@ -4,133 +4,106 @@ import argparse
 import logging
 import asyncio
 
-from aiogram import Bot, Dispatcher, types
+from aiogram import Bot, Dispatcher
+from aiogram.filters import Command
+from aiogram.types import Message
 
 from disney import DisneyPlus, Data
 from config import token, users, groups
-
-
-class MyArgumentParser(argparse.ArgumentParser):
-    def __init__(self, *args, **kwargs):
-        super(MyArgumentParser, self).__init__(*args, **kwargs)
-
-        self.error_message = ""
-
-    def error(self, message):
-        self.error_message = message
-
-    def parse_args(self, *args, **kwargs):
-        # catch SystemExit exception to prevent closing the application
-        result = None
-        try:
-            result = super().parse_args(*args, **kwargs)
-        except SystemExit:
-            pass
-        return result
 
 
 API_TOKEN = token
 USERS = users
 GROUPS = groups
 
-# Configure logging
-logging.basicConfig(level=logging.INFO)
-
-# Initialize bot and dispatcher
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher()
 
+class MyArgumentParser(argparse.ArgumentParser):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.error_message = ""
 
-async def log(commands, message: types.Message):
-    group_name = message["chat"]["title"]
-    from_user = message["from"]
+    def error(self, message):
+        self.error_message = message
 
-    if group_name := message["chat"]["title"]:
-        bot.logging.info(f"Group: {group_name}")
-    if from_user := message["from"]:
-        bot.logging.info(f'User: {from_user["username"] or from_user["first_name"]}')
+    def parse_args(self, *args, **kwargs):
+        # Catch SystemExit exception to prevent closing the application
+        try:
+            return super().parse_args(*args, **kwargs)
+        except SystemExit:
+            return None
 
-    bot.logging.info(f"Commands: {commands}")
+
+async def log(commands, message: Message):
+    group_name = message.chat.title
+    from_user = message.from_user
+
+    if group_name:
+        logging.info(f"Group: {group_name}")
+    if from_user:
+        logging.info(f"User: {from_user.username or from_user.first_name}")
+
+    logging.info(f"Commands: {commands}")
 
 
-async def eligible(commands, message: types.Message):
+async def eligible(commands, message: Message):
     await log(commands, message)
 
-    if message["chat"]["id"] in GROUPS:
-        return True
-    elif message["from"]["id"] in USERS:
+    if message.chat.id in GROUPS or message.from_user.id in USERS:
         return True
     else:
         await message.reply("You are not eligible to use this bot!")
         return False
 
 
-@dp.message_handler(commands=["start"])
-async def send_welcome(message: types.Message):
-    """
-    This handler will be called when user sends `/start` command
-    """
-    bot.logging.info(f'User: {message["from"]["username"]}')
-    bot.logging.info("Commands: start")
-    await message.reply(
-        "Hi!\nI'm a Disney Plus information grabber bot! If you want to know which series are available at a specific region, this bot is for you!"
+@dp.message(Command("start"))
+async def send_welcome(message: Message):
+    """Handles `/start` command."""
+    logging.info(f"User: {message.from_user.username}")
+    logging.info("Commands: start")
+    await message.answer(
+        "Hi!\nI'm a Disney Plus information grabber bot! "
+        "If you want to know which series are available in a specific region, this bot is for you!"
     )
 
 
-@dp.message_handler(commands=["groupid"])
-async def send_groupid(message: types.Message):
-    """
-    Sends the group ID to the user.
-    """
+@dp.message(Command("groupid"))
+async def send_groupid(message: Message):
+    """Sends the group ID to the user."""
     await log("groupid", message)
 
-    await message.reply(
-        f"<b>Group ID: <code>{message['chat']['id']}</code></b>",
+    await message.answer(
+        f"<b>Group ID: <code>{message.chat.id}</code></b>",
         parse_mode="html",
         disable_web_page_preview=True,
     )
 
 
-@dp.message_handler(commands=["userid"])
-async def send_userid(message: types.Message):
-    """
-    Sends the user ID to the user.
-    """
+@dp.message(Command("userid"))
+async def send_userid(message: Message):
+    """Sends the user ID to the user."""
     await log("userid", message)
 
-    await message.reply(
-        f"<b>Your User ID: <code>{message['from']['id']}</code></b>",
+    await message.answer(
+        f"<b>Your User ID: <code>{message.from_user.id}</code></b>",
         parse_mode="html",
         disable_web_page_preview=True,
     )
 
 
-@dp.message_handler(commands=["usage", "help"])
-async def send_help(message: types.Message):
-    """
-    This handler will be called when user sends `/usage` or `/help` command
-    """
+@dp.message(Command("usage", "help"))
+async def send_help(message: Message):
+    """Handles `/usage` or `/help` command."""
     await log("help", message)
 
-    await message.reply(
+    await message.answer(
         """
 <b>Usage:</b>
 <code>/check [-r &lt;regions&gt;] [-s &lt;num&gt;] [-q &lt;value&gt;] [-al &lt;lang&gt;] [-sl &lt;lang&gt;] &lt;url&gt;</code>
 
 Finds which regions a movie or series is available in on Disney+.
 For TV shows, also returns a list of seasons and the number of matching episodes in each season.
-
-<b>Options:</b>
-<code>-r </code>/<code> --regions</code>
-Comma-separated list of 2-letter country codes to limit the search to. Default is to check all regions.
-<code>-s </code>/<code> --season</code>
-Limit search to the specified season(s). Default is to check all seasons. (Examples: -s 1, -s 1-2)
-<code>-q </code>/<code> --quality</code>
-Only show movies/episodes that have the specified quality. Possible values are SD, HD, UHD.
-<code>-al </code>/<code> --alang</code>
-Only show movies/episodes that have the specified audio track (2-letter language code).
-<code>-sl </code>/<code> --slang</code>
-Only show movies/episodes that have the specified subtitle track (2-letter language code).
 
 <b>Example:</b>
 <code>/check -r us,fr -sl pl -al pl https://www.disneyplus.com/movies/star-wars-attack-of-the-clones-episode-ii/mgpYHGnzZW6N</code>
@@ -140,111 +113,83 @@ Only show movies/episodes that have the specified subtitle track (2-letter langu
     )
 
 
-@dp.message_handler(commands=["regions"])
-async def send_regions(message: types.Message):
-    """
-    This handler will be called when user sends `/regions` command
-    """
-    await log("help", message)
+@dp.message(Command("regions"))
+async def send_regions(message: Message):
+    """Handles `/regions` command."""
+    await log("regions", message)
 
-    await message.reply(
+    await message.answer(
         f"All the available regions ({len(bot.disney.regions)}):\n<code>{', '.join(bot.disney.regions)}</code>",
         parse_mode="html",
     )
 
 
-@dp.message_handler(commands=["check"])
-async def send_check(message: types.Message):
-    """
-    This handler will be called when user sends `/check` command
-    """
-    is_ligible = await eligible("check", message)
-    if not is_ligible:
-        return False
+@dp.message(Command("check"))
+async def send_check(message: Message):
+    """Handles `/check` command."""
+    if not await eligible("check", message):
+        return
 
-    parser = MyArgumentParser(
-        argparse.ArgumentParser(description="DSNPbot", prog="/check")
-    )
-    parser.add_argument(
-        "-sl",
-        "--slang",
-        type=str,
-        default=None,
-    )
-    parser.add_argument(
-        "-al",
-        "--alang",
-        type=str,
-        default=None,
-    )
-    parser.add_argument(
-        "-ml",
-        "--mlang",
-        type=str,
-        default=None,
-    )
-    parser.add_argument(
-        "-r",
-        "--regions",
-        type=str,
-        default=None,
-    )
-    parser.add_argument(
-        "-q",
-        "--quality",
-        type=str,
-        default=None,
-    )
-    parser.add_argument(
-        "-s",
-        "--seasons",
-        type=str,
-        default=None,
-    )
-    parser.add_argument(
-        "url",
-        type=str,
-        default=None,
-    )
+    parser = MyArgumentParser(description="DSNPbot", prog="/check")
+    parser.add_argument("-sl", "--slang", type=str, default=None)
+    parser.add_argument("-al", "--alang", type=str, default=None)
+    parser.add_argument("-ml", "--mlang", type=str, default=None)
+    parser.add_argument("-r", "--regions", type=str, default=None)
+    parser.add_argument("-q", "--quality", type=str, default=None)
+    parser.add_argument("-s", "--seasons", type=str, default=None)
+    parser.add_argument("url", type=str, default=None)
 
-    message_in: str = message.get_args() or ""
-    args = parser.parse_args(message_in.split())
+    message_text: str = message.text or ""
+    args = parser.parse_args(message_text.split()[1:])  # Remove "/check" from arguments
+
     if parser.error_message:
-        await message.reply(parser.error_message, disable_web_page_preview=True)
-        del message_in
-    elif message_in and "http" in message_in:
+        await message.answer(parser.error_message, disable_web_page_preview=True)
+        return
+
+    if args and "http" in args.url:
         if args.quality and args.quality.upper() not in ["SD", "HD", "UHD"]:
-            await message.reply("Error: Invalid quality!")
+            await message.answer("Error: Invalid quality!")
         else:
-            bot.logging.info(f"URL: {args.url}")
-            sent_message: types.Message = await message.reply("Checking...")
+            logging.info(f"URL: {args.url}")
+            sent_message = await message.answer("Checking...")
+
             if "browse/entity" in args.url:
-                await sent_message.edit_text("Error: Entity url detected, only old url can be used!")
-                bot.logging.warning("Error: Entity url detected, only old url can be used!")
+                await sent_message.edit_text(
+                    "Error: Entity URL detected, only old URLs can be used!"
+                )
+                logging.warning(
+                    "Error: Entity URL detected, only old URLs can be used!"
+                )
                 return
+
             try:
                 data = Data(args, sent_message, bot)
                 if data.id:
                     await bot.disney.get_available(data)
-                    bot.logging.info(f"Finished: {data.id}")
+                    logging.info(f"Finished: {data.id}")
                 else:
-                    await sent_message.edit_text("Error: Failed to get title id!")
-                    bot.logging.warning("Error: Failed to get title id!")
+                    await sent_message.edit_text("Error: Failed to get title ID!")
+                    logging.warning("Error: Failed to get title ID!")
             except Exception as e:
                 await sent_message.edit_text(
-                    f"Error: {e}",
-                    disable_web_page_preview=True,
+                    f"Error: {e}", disable_web_page_preview=True
                 )
-                bot.logging.error(f"Error: {e}")
+                logging.error(f"Error: {e}")
     else:
-        await message.reply("Error: No usable input!")
+        await message.answer("Error: No usable input!")
 
 
 async def main():
+    """Bot startup function."""
+
+    logging.basicConfig(level=logging.INFO)
+
+    logging.info("Starting bot...")
     bot.logging = logging.getLogger("DSNPbot")
     bot.disney = DisneyPlus(bot)
-    await bot.disney.init_session()
+    await bot.disney.init_session(bot)
     await dp.start_polling(bot)
+
 
 if __name__ == "__main__":
     asyncio.run(main())
